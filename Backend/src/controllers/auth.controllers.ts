aceacdbe495dbb6a475145e09b40372eb4bdc409
2iaad <ziadderfoufi77@gmail.com>
2025-12-19 07:00:15 +0100

    [hashing password to store user infos inside MongoDB]

--- Backend/src/controllers/auth.controllers.ts
+++ Backend/src/controllers/auth.controllers.ts
@@ -3,26 +3,48 @@
  */
 
 import type { Request, Response } from "express";
+import User from '../models/user.model.ts'
+import bcrypt from "bcryptjs"
 
-export function signup(req: Request, res: Response)
-{
-    // const fullName = req.body; // This line creates three separate variables, each one takes its value Ex: email = req.body.email
-    console.log(req.body)
-    // try {
+export async function signup(req: Request, res: Response) {
+    const { fullName, email, password } = req.body; // This line creates three separate variables, each one takes its value Ex: email = req.body.email
+    console.log("here1")
+    try {
+        if (password.length < 6) {
+            console.log("Password too short, exiting");
+            return res.status(400).json({message: "Password must be atleast 6 characters"})
+        }
         
-    // } catch (error) {
+        const user = await User.findOne({email}) // looks for a document with this email inside the collection, then it return the document found || null, {email} stands for {email: ".."} its an object that we search for 
+        if (user) {
+            
+            console.log("User error");
+            return res.status(400).json({message: "User already exists"})
+        }
+
+        console.log("here2")
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         
-    // }
-    
-    res.status(200).send('<h1>SignUp rout<h1>')
+        console.log(hashedPassword)
+        const newUser = new User({
+            fullName: fullName,
+            email,
+            password: hashedPassword
+        })
+        console.log(newUser)
+        
+    } catch (error) {
+        
+    }
+    console.log("here3")
+    return res.status(200).json({message: "GOOD!"})
 }
 
-export const login = (req:Request, res:Response) =>
-{
+export const login = (req: Request, res: Response) => {
     res.send("login rout")
 }
 
-export const logout = (req:Request, res:Response) =>
-{
+export const logout = (req: Request, res: Response) => {
     res.send("logout rout")
 }
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -26,5 +26,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
-    // connectDB()
+    connectDB()
 })
