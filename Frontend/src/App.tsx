f8c848b129dd707c008f331549a0bc599cab7aac
2iaad <ziadderfoufi77@gmail.com>
2025-12-24 20:40:13 +0100

    [init] installing tailwindcss + daisyui

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+
+	return (
+		<>
+			<button className="btn">Button</button>
+			<div className="chat chat-start">
+				<div className="chat-bubble">
+					It's over Anakin,
+					<br />
+					I have the high ground.
+				</div>
+				</div>
+				<div className="chat chat-end">
+				<div className="chat-bubble">You underestimate my power!</div>
+			</div>
+		</>
+	)
+}
